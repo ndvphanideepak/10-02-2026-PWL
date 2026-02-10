c4a642e29b4ef748f523162b67cb9b0b00d1268e
ndvphanideepak <ndvphanideepak@gmail.com>
2026-02-10 07:45:21 +0530

    PlaywrightLearning

---
+++ Day1/comments.js
@@ -0,0 +1,12 @@
+
+// console.log("Comments.js loaded");
+/**
+ * Adds two numbers
+ * @param {*} a 
+ * @param {*} b 
+ * @returns  sum of 2 numbers
+ */
+function add(a , b)
+{
+    return a + b;//return type is a number
+}
---
+++ Day1/vraibale.js
@@ -0,0 +1,15 @@
+//{var|let|const} {varaibaleName} [=value]
+
+let greeting = "Hello World";
+console.log(greeting);
+
+//multiple variables in same line using ,
+let  fisrtName = "Phani" , lastName  = "Deepak"
+console.log(fisrtName, lastName);
+
+//declaring variable without value is undefined
+let age;
+console.log(age);
+
+//var let and const
+// let and const is used 
