d09a7cfd7d9227111d0e52f2b65df9c5fbee6ae5
ndvphanideepak <ndvphanideepak@gmail.com>
2026-02-11 08:00:46 +0530

    Converstions and Operators

---
+++ Day2/Parse.js
@@ -0,0 +1,12 @@
+let val = "5";
+numval1 = parseInt(val);
+numval2 = parseFloat(val);
+
+console.log(numval1);
+console.log(numval2);
+console.log(`the type of numval1 is ${typeof numval1}`);
+console.log(`the type of numval2 is ${typeof numval2}`);
+
+strvalue = numval1.toString();
+console.log(strvalue);
+console.log(`the type of strvalue is ${typeof strvalue}`);
---
+++ Day2/Ternary.js
@@ -0,0 +1,8 @@
+let a = "hello";
+let value = a ? "true" : "false";
+console.log(value);
+
+let b = 20;
+let c = 10;
+let result = b > c ? "b is greater than c" : "b is not greater than c";
+console.log(result);
